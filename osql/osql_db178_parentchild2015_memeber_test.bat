4a433536380969f2e7416ac49e4746c3adacfc97
sjxhot <34635256+sjxhot@users.noreply.github.com>
2021-03-09 21:24:24 +0800

    osql

---
+++ osql/osql_db178_parentchild2015_memeber_test.bat
@@ -0,0 +1,18 @@
+.:user id=user134;password=1qaz@WSX
+:h:\a.sql �N�令�ڵ��A��SQL
+
+:osql -U user134 -P 1qaz@WSX -S [172.25.142.178] -d SOMTDB -i a.sql
+:osql -U user134 -P 1qaz@WSX -S [172.25.142.178] -d SOMTDB -i a.sql
+
+
+
+:osql -U sa -P sa -d test -i "NursemaidInfo - test_BK.txt"
+:osql -U sa -P sa -d test -i NursemaidInfo_test.sql
+osql -U user134 -P 1qaz@WSX -S 172.25.142.178 -d SOMTDB -i parentchild2015_memeber.txt
+
+:
+
+
+pause
+
+
